adb9708bb534aa0b67279d126ffe833684799be6
naveen1992d13-byte <naveen1992d13@gmail.com>
2026-07-30 22:17:36 +0530

    Add files via upload

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App.js";
+import "@/index.css";
+import App from "@/App";
 
-const rootElement = document.getElementById("root");
-
-if (!rootElement) {
-  throw new Error('Root element with id "root" was not found');
-}
-
-const root = ReactDOM.createRoot(rootElement);
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
